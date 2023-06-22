93c5e4592141abbc6942a15d397fe1db8eb744d7
David He <31311090+iteemhe@users.noreply.github.com>
2023-06-22 15:15:27 -0700

    update config

--- lua/plugins/config/barbar.lua
+++ lua/plugins/config/barbar.lua
@@ -28,7 +28,7 @@ local config = {
         -- Configure the base icons on the bufferline.
         buffer_index = false,
         buffer_number = false,
-        button = "",
+        button = "󰖭",
         -- Enables / disables diagnostic symbols
         diagnostics = {
             { enabled = true, icon = " " }, -- ERROR
@@ -47,7 +47,7 @@ local config = {
         -- Configure the icons on the bufferline when modified or pinned.
         -- Supports all the base icon options.
         modified = { button = "●" },
-        pinned = { button = "車" },
+        pinned = { button = "󰐃" },
         -- Configure the icons on the bufferline based on the visibility of a buffer.
         -- Supports all the base icon options, plus `modified` and `pinned`.
         alternate = { filetype = { enabled = false } },
--- lua/plugins/config/indent-blankline.lua
+++ lua/plugins/config/indent-blankline.lua
@@ -1,5 +1,5 @@
 local config = {
-    show_end_of_line = true,
+    show_end_of_line = false,
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = true,
